3b1a4b3af0c83662fbb0892ec5aa5037fcd1ff4b
Ratan Mia <ratan.kawasaki@gmail.com>
2024-03-20 15:45:18 +0600

    Sorting tyre size

--- app/Http/Controllers/WarrantyClaimController.php
+++ app/Http/Controllers/WarrantyClaimController.php
@@ -26,10 +26,10 @@ class WarrantyClaimController extends Controller
     {
         $vehicleType = VehicleType::where(['slug' => 'car'])->first();
         $cities = City::orderBy('name', 'asc')->get();
-        $products = Product::where('vehicle_type_id','1')->get();
-        $retailers = Retailer::where('vehicle_type_id','1')->get();
-        $tyreSizes = ProductSize::all();
-        return view('warranty-register.warranty-claim', compact('products','tyreSizes', 'retailers', 'cities'));
+        $products = Product::where('vehicle_type_id', '1')->get();
+        $retailers = Retailer::where('vehicle_type_id', '1')->get();
+        $tyreSizes = ProductSize::all()->sortByDesc('size');
+        return view('warranty-register.warranty-claim', compact('products', 'tyreSizes', 'retailers', 'cities'));
     }
 
 
@@ -41,21 +41,21 @@ class WarrantyClaimController extends Controller
 
         // dd($request);
         $product_name = Product::select('name')
-            ->where('id',$request->product_name_id)
+            ->where('id', $request->product_name_id)
             ->first();
 
         $product_size = ProductSize::select('size')
-            ->where('id',$request->product_size_id)
+            ->where('id', $request->product_size_id)
             ->first();
 
 
 
         $city = City::select('name')
-            ->where('id',$request->city)
+            ->where('id', $request->city)
             ->first();
 
         $retailer = Retailer::select('name')
-            ->where('id',$request->retailer_id)
+            ->where('id', $request->retailer_id)
             ->first();
 
         $data = [
@@ -72,7 +72,7 @@ class WarrantyClaimController extends Controller
             'DOT' => $request->product_dot,
             'Product_Size' => $product_size->size,
             'Retailer_Area1' => $request->quantity_purchased,
-//            'Vendor_Name' => $retailer->name,
+            //            'Vendor_Name' => $retailer->name,
             'Retailer_Name' => $retailer->name,
             'Email_Opt_Out' => $request->subscribed ? false : true,
         ];
@@ -83,42 +83,42 @@ class WarrantyClaimController extends Controller
 
         // Save into database
 
-//        $carRegistration = new CarRegistration();
-//        $carRegistration->first_name = $request->first_name;
-//        $carRegistration->last_name = $request->last_name;
-//        $carRegistration->email = $request->email;
-//        $carRegistration->phone = $request->phone;
-//        $carRegistration->city_id = $request->city;
-//        $carRegistration->address = $request->address;
-//        $carRegistration->zip = $request->zip;
-//        $carRegistration->date_purchased = Carbon::createFromFormat('Y-m-d', $request->date_purchased)->format('d-m-Y');
-//        $carRegistration->product_name_id = $request->product_name_id;
-//        $carRegistration->invoice_number = $request->invoice_number;
-//        $carRegistration->product_size_id = $request->product_size;
-//        $carRegistration->product_dot = $request->product_dot;
-//        $carRegistration->product_quantity = $request->quantity_purchased;
-//
-//        $carRegistration->warranty_number = Str::random(10);
-//        $carRegistration->save();
+        //        $carRegistration = new CarRegistration();
+        //        $carRegistration->first_name = $request->first_name;
+        //        $carRegistration->last_name = $request->last_name;
+        //        $carRegistration->email = $request->email;
+        //        $carRegistration->phone = $request->phone;
+        //        $carRegistration->city_id = $request->city;
+        //        $carRegistration->address = $request->address;
+        //        $carRegistration->zip = $request->zip;
+        //        $carRegistration->date_purchased = Carbon::createFromFormat('Y-m-d', $request->date_purchased)->format('d-m-Y');
+        //        $carRegistration->product_name_id = $request->product_name_id;
+        //        $carRegistration->invoice_number = $request->invoice_number;
+        //        $carRegistration->product_size_id = $request->product_size;
+        //        $carRegistration->product_dot = $request->product_dot;
+        //        $carRegistration->product_quantity = $request->quantity_purchased;
+        //
+        //        $carRegistration->warranty_number = Str::random(10);
+        //        $carRegistration->save();
 
-            $warrantyClaim = new WarrantyClaim();
-            $warrantyClaim->first_name = $request->first_name;
-            $warrantyClaim->last_name = $request->last_name;
-            $warrantyClaim->email = $request->email;
-            $warrantyClaim->phone = $request->phone;
-            $warrantyClaim->city_id = $request->city;
-            $warrantyClaim->address = $request->address;
-            $warrantyClaim->invoice_number = $request->invoice_number;
-            $warrantyClaim->product_name_id = $request->product_name_id;
-            $warrantyClaim->product_size_id = $request->product_size_id;
-            $warrantyClaim->warranty_number = Str::random(10);
-
-
+        $warrantyClaim = new WarrantyClaim();
+        $warrantyClaim->first_name = $request->first_name;
+        $warrantyClaim->last_name = $request->last_name;
+        $warrantyClaim->email = $request->email;
+        $warrantyClaim->phone = $request->phone;
+        $warrantyClaim->city_id = $request->city;
+        $warrantyClaim->address = $request->address;
+        $warrantyClaim->invoice_number = $request->invoice_number;
+        $warrantyClaim->product_name_id = $request->product_name_id;
+        $warrantyClaim->product_size_id = $request->product_size_id;
+        $warrantyClaim->warranty_number = Str::random(10);
 
 
-            $warrantyClaim->save();
 
-        if($request->file('photos')) {
+
+        $warrantyClaim->save();
+
+        if ($request->file('photos')) {
             foreach ($request->file('photos') as $photo) {
                 $warrantyClaim->addMedia($photo)->toMediaCollection('photos');
             }
@@ -137,19 +137,19 @@ class WarrantyClaimController extends Controller
 
 
 
-            if($response->data[0]->status == 'error') {
+            if ($response->data[0]->status == 'error') {
                 return Redirect::back()->with('status', 'There has been some error and your data could not be submitted.. `Please try again.');
-//                return redirect(route('warranty-register-moto-error'));
-//                dd($response);
+                //                return redirect(route('warranty-register-moto-error'));
+                //                dd($response);
             }
 
             $id = $response->data[0]->details->id;
 
 
 
-            if($request->file('photos')) {
+            if ($request->file('photos')) {
                 foreach ($request->file('photos') as $photo) {
-//                    $response = $api->uploadFile($id, $photo);
+                    //                    $response = $api->uploadFile($id, $photo);
                 }
             }
 
@@ -158,16 +158,15 @@ class WarrantyClaimController extends Controller
             if ($file) {
                 $response = $api->uploadFile($id, $file);
                 $warrantyClaim->addMedia($request->file('invoice_attachment'))->toMediaCollection('invoice_attachment');
-
             }
 
-            if($response == 'success') {
-//                return redirect(route('warranty-register-car-success'));
+            if ($response == 'success') {
+                //                return redirect(route('warranty-register-car-success'));
                 return Redirect::back()->with('status', 'Warranty claim application submitted successfully. `Pirelli team will contact you soon.');
             }
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             dd($e);
-//            return redirect(route('warranty-register-moto-error'));
+            //            return redirect(route('warranty-register-moto-error'));
             return Redirect::back()->with('status', 'There has been some error and your data could not be submitted.. `Please try again.');
         }
     }
