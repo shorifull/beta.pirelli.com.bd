77e48bba18ef68e636b61a76c58819974da346ad
Ratan Mia <ratan.kawasaki@gmail.com>
2024-03-20 15:39:53 +0600

    Update warranty-claim.blade.php

--- resources/views/warranty-register/warranty-claim.blade.php
+++ resources/views/warranty-register/warranty-claim.blade.php
@@ -325,7 +325,7 @@
 
                     <div class="find-out-more-btn">
 
-                        <a style="text-align:center;" href="{{asset('Pirelli Tyre Warranty Policy PDF - Download 1111.pdf')}}">
+                        <a style="text-align:center;" target="_blank" href="{{asset('Pirelli Tyre Warranty Policy PDF - Download 1111.pdf')}}">
                             <button class="costum-btn">Download Manuals</button>
                         </a>
                     </div>
