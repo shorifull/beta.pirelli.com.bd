1c13633b9d6067b23447632a288de6eed634df23
Ratan Mia <ratan.kawasaki@gmail.com>
2024-07-04 16:35:26 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,11 +26,11 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://www.google.com/recaptcha/api.js"></script>
-    <script>
+    <!-- <script>
         function onSubmit(token) {
             document.getElementById("warrantyClaim").submit();
         }
-    </script>
+    </script> -->
 
 </body>
 
